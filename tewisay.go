f4536baa1472a6c19dd25a3f6e1ff76ec332b61b
lucy <ne.tetewi@gmail.com>
2015-11-29 13:32:39 +0100

    fix deps

--- tewisay.go
+++ tewisay.go
@@ -1,12 +1,12 @@
-/* tewisay - cowsay clone
-
-To the extent possible under law, the author(s) have dedicated all
-copyright and related and neighboring rights to this software to the public
-domain worldwide. This software is distributed without any warranty.
-
-You should have received a copy of the CC0 Public Domain Dedication along
-with this software.
-If not, see <http://creativecommons.org/publicdomain/zero/1.0/>. */
+// tewisay is a cowsay clone
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to the public
+// domain worldwide. This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along
+// with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 package main
 
@@ -17,8 +17,8 @@ import (
 	"path"
 	"strings"
 
-	flag "./deps/pflag"
-	"./deps/rwidth"
+	flag "github.com/lucy/pflag"
+	"github.com/lucy/runewidth"
 )
 
 var (
@@ -87,7 +87,7 @@ func countRunes(s string) int {
 		if r == '\t' {
 			n += 8 - (n % 8)
 		} else {
-			n += rwidth.Width(r)
+			n += runewidth.RuneWidth(r)
 		}
 	}
 	return n
